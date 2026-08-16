1d50b7e775baba8a8bb53faeb4582ec4353cdbcd
HT <hieutruong6219@gmail.com>
2026-08-16 13:15:23 -0700

    Init
    5th times the charm. I got a flake for version control with home manager
    for managin the user configs + config files.

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "ZaneyOS";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } : let
+    lib = nixpkgs.lib;
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        inherit system;
+        modules = [ ./configuration.nix ];
+      };
+    };
+    
+    homeConfigurations = {
+      nyx0 = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
+      };
+    };
+  };
+}
